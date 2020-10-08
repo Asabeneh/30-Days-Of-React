af0f5bae639ee8c3ead5c8ccd0a78598967e3f68
Derrek Gass <dardcore@gmail.com>
2020-10-08 12:49:07 -0700

    moving into variables

--- solutions/day-01/main.js
+++ solutions/day-01/main.js
@@ -1,9 +1,36 @@
-let firstName = 'Asabeneh'
-firstName = 'Eyob'
-
+let firstName = 'Derrek'
+firstName = 'Deek'
+console.log(firstName)
 const PI = 3.14 // Not allowed to reassign PI to a new value
 // PI = 3.
 
 
 const arr = []
 console.log(arr)
+
+const numbers = [0, 3.14, 9.81, 37, 98.6, 100] // array of numbers
+const fruits = ['banana', 'orange', 'mango', 'lemon'] // array of strings, fruits
+const vegetables = ['Tomato', 'Potato', 'Cabbage', 'Onion', 'Carrot'] // array of strings, vegetables
+const animalProducts = ['milk', 'meat', 'butter', 'yogurt'] // array of strings, products
+const webTechs = ['HTML', 'CSS', 'JS', 'React', 'Redux', 'Node', 'MongDB'] // array of web technologies
+const countries = ['Finland', 'Denmark', 'Sweden', 'Norway', 'Iceland'] // array of strings, countries
+
+// Print the array and its length
+
+console.log('Numbers:', numbers)
+console.log('Number of numbers:', numbers.length)
+
+console.log('Fruits:', fruits)
+console.log('Number of fruits:', fruits.length)
+
+console.log('Vegetables:', vegetables)
+console.log('Number of vegetables:', vegetables.length)
+
+console.log('Animal products:', animalProducts)
+console.log('Number of animal products:', animalProducts.length)
+
+console.log('Web technologies:', webTechs)
+console.log('Number of web technologies:', webTechs.length)
+
+console.log('Countries:', countries)
+console.log('Number of countries:', countries.length)
