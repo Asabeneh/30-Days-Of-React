e3b67eeb79d9a1a0cb137b782ca0a7cc2b6dcbbf
Derrek Gass <dardcore@gmail.com>
2020-10-08 15:28:38 -0700

    begin exercise 1

---
+++ solutions/day-01/exercise1.js
@@ -0,0 +1,23 @@
+const countries = [
+    'Albania',
+    'Bolivia',
+    'Canada',
+    'Denmark',
+    'Ethiopia',
+    'Finland',
+    'Germany',
+    'Hungary',
+    'Ireland',
+    'Japan',
+    'Kenya',
+  ]
+  
+  const webTechs = [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Redux',
+    'Node',
+    'MongoDB',
+  ]
