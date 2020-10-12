d86dd160f29e14ec50c69a4c705c73e59a6486f2
Derrek Gass <dardcore@gmail.com>
2020-10-12 13:48:38 -0700

    global scope

--- solutions/day-01/scope.js
+++ solutions/day-01/scope.js
@@ -1,4 +1,5 @@
 //scope.js
+/*
 a = 'JavaScript' // is a window scope this found anywhere
 b = 10 // this is a window scope variable
 function letsLearnScope() {
@@ -7,3 +8,38 @@ function letsLearnScope() {
         console.log(a, b)
     }
 }
+*/
+/*
+let a = 'JavaScript' // is a global scope it will be found anywhere in this file
+let b = 10 // is a global scope it will be found anywhere in this file
+function letsLearnScope() {
+  console.log(a, b) // JavaScript 10, accessible
+  if (true) {
+    let a = 'Python'
+    let b = 100
+    console.log(a, b) // Python 100
+  }
+  console.log(a, b)
+}
+letsLearnScope()
+console.log(a, b) // JavaScript 10, accessible
+*/
+
+let a = 'JavaScript' // is a global scope it will be found anywhere in this file
+let b = 10 // is a global scope it will be found anywhere in this file
+function letsLearnScope() {
+  console.log(a, b) // JavaScript 10, accessible
+  let c = 30
+  if (true) {
+    // we can access from the function and outside the function but
+    // variables declared inside the if will not be accessed outside the if block
+    let a = 'Python'
+    let b = 20
+    let d = 40
+    console.log(a, b, c) // Python 20 30
+  }
+  // we can not access c because c's scope is only the if block
+  console.log(a, b) // JavaScript 10
+}
+letsLearnScope()
+console.log(a, b) // JavaScript 10, accessible
