1739d9bfcd73f196db3adfa778967ad8e9fbaa07
Derrek Gass <dardcore@gmail.com>
2020-10-12 13:44:32 -0700

    working on scope day1

---
+++ solutions/day-01/scope.js
@@ -0,0 +1,8 @@
+//scope.js
+a = 'JavaScript' // is a window scope this found anywhere
+b = 10 // this is a window scope variable
+function letsLearnScope() {
+  console.log(a, b)
+  if (true) {
+    console.log(a, b)
+  }
