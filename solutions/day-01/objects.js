c61be332dc507915be4bd592cac370ac9a199bef
Derrek Gass <dardcore@gmail.com>
2020-10-12 14:08:12 -0700

    objects

---
+++ solutions/day-01/objects.js
@@ -0,0 +1,21 @@
+const person = {
+    firstName: 'Asabeneh',
+    lastName: 'Yetayeh',
+    age: 250,
+    country: 'Finland',
+    city: 'Helsinki',
+    skills: [
+      'HTML',
+      'CSS',
+      'JavaScript',
+      'React',
+      'Node',
+      'MongoDB',
+      'Python',
+      'D3.js',
+    ],
+    getFullName: function () {
+      return `${this.firstName}${this.lastName}`
+    },
+    'phone number': '+3584545454545',
+  }
