5c9efd68e17b00ba755ac0c48fabe32d97ff232e
11Ashish11 <ashish.bhoya.56@gmail.com>
2021-11-07 21:23:22 +0530

    Read some random JS stuff

---
+++ 01_Day_JavaScript_Refresher/first.js
@@ -0,0 +1,24 @@
+let arr = [];
+console.log(arr);
+let secondArr = [5,4,3,2,1];
+secondArr.sort();
+console.log(secondArr);
+
+let check = 1;
+
+function tryingHere(){
+    const trying = "This is Ashish Bhoya";
+    const MikeRoss = "This is legendary Mike Ross";
+    if(check ===1)
+    document.getElementById("Ok").innerHTML = trying , check  = 0;
+    else
+    document.getElementById("Ok").innerHTML = MikeRoss, check = 1;
+}
+
+function table(){
+    let num = document.getElementById("input").value;
+    document.getElementById("tableHere").innerHTML = "";        
+    for(let i = 1; i<=10; i++){
+        document.getElementById("tableHere").innerHTML += `${num} * ${i} = ${num*i} <br>`;
+    }
+}
