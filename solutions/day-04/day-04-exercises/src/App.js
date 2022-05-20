85a31d937923265cef2db4b82ea551957e8118ef
diegobaena89 <diegobf.cello@gmail.com>
2022-05-20 16:24:20 -0300

    exercise level 3 - 1 added

--- solutions/day-04/day-04-exercises/src/App.js
+++ solutions/day-04/day-04-exercises/src/App.js
@@ -1,12 +1,18 @@
 import React from 'react'
-import Background from './pages/Background'
+// import Background from './pages/Background'
 import './App.css'
+import Stripe from './pages/Stripe'
 
 
-const App = () => {
+const App = (props) => {
   return (
     <div>
-        <Background />
+        {/* <Background /> */}
+        <Stripe bgColor="#518cef" color="#518cef" />
+        <Stripe bgColor="#3b10c4" color="#3b10c4" />
+        <Stripe bgColor="#9aede6" color="#9aede6" />
+        <Stripe bgColor="#8ee763" color="#8ee763" />
+        <Stripe bgColor="#a30dd0" color="#a30dd0" />
     </div>
   )
 }
