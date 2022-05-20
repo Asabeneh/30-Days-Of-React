d79d7d85d5611b66764f2072c1a03d435a9aba79
diegobaena89 <diegobf.cello@gmail.com>
2022-05-19 21:19:01 -0300

    <feat> exercise 2 from day 4 added

---
+++ solutions/day-04/day-04-exercises/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Background from './pages/Background'
+import './App.css'
+
+
+const App = () => {
+  return (
+    <div>
+        <Background />
+    </div>
+  )
+}
+
+export default App
