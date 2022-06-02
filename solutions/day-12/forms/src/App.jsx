7680b85831140d85974a8c882db19c06ad7f6041
diegobaena89 <diegobf.cello@gmail.com>
2022-06-01 22:21:41 -0300

    day-12 added

---
+++ solutions/day-12/forms/src/App.jsx
@@ -0,0 +1,91 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+class App extends Component {
+  // declaring initial state
+  state = {
+    firstName: '',
+    lastName: '',
+    country: '',
+    title: '',
+  }
+  handleChange = (e) => {
+    /*
+    we can get the name and value like this: e.target.name, e.target.value
+    but we can also destructure  name and value from e.target
+    const name = e.target.name
+    const value = e.target.value
+    */
+    const { name, value } = e.target
+    // [variablename] to use a variable name as a key in an object
+    // name refers to the name attribute of the input elements
+    this.setState({ [name]: value })
+  }
+  handleSubmit = (e) => {
+    /* 
+     e.preventDefault()
+      stops the default behavior of form element
+     specifically refreshing of page
+     */
+    e.preventDefault()
+
+    /*
+     the is the place where we connect backend api 
+     to send the data to the database
+     */
+
+    console.log(this.state)
+  }
+
+  render() {
+    // accessing the state value by destrutcturing the state
+    const { firstName, lastName, title, country } = this.state
+    return (
+      <div className='App'>
+        <h3>Add Student</h3>
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <input
+              type='text'
+              name='firstName'
+              placeholder='First Name'
+              value={firstName}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div>
+            <input
+              type='text'
+              name='lastName'
+              placeholder='Last Name'
+              value={lastName}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div>
+            <input
+              type='text'
+              name='country'
+              placeholder='Country'
+              value={country}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div>
+            <input
+              type='text'
+              name='title'
+              placeholder='Title'
+              value={title}
+              onChange={this.handleChange}
+            />
+          </div>
+
+          <button class='btn btn-success'>Submit</button>
+        </form>
+      </div>
+    )
+  }
+}
+
+
+export default App
