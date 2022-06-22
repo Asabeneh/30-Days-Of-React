029bd5477bb57f09ee1a61d9ecba6dee0d913da5
Abhishek Ranjan <aviranjan444@gmail.com>
2022-06-22 20:14:40 +0530

    Day 1

---
+++ my-test-solutions/test-react-project/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import './index.css';
+import Navbar from './Navigation/Navbar.js';
+
+function App() {
+  return (
+    <div>
+    <Navbar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-test-solutions/test-react-project/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/App.js", "./src/index.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
