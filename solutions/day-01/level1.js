bf96edf710411a56fc247c2373115216e3915e9e
jheave <jimenezjheave123@gmail.com>
2022-07-13 00:36:07 +0800

    add comment

--- solutions/day-01/level1.js
+++ solutions/day-01/level1.js
@@ -3,7 +3,10 @@ const itCompanies = ['Facebook', 'Google', 'Microsoft', 'Apple','IBM', 'Oracle',
 
 // display array of it companies
 console.log(itCompanies);
+
+// display length of it companies
 console.log(`length of IT companies ${itCompanies.length}`)
+
 console.log(`${itCompanies[0]}, ${itCompanies[(itCompanies.length - 1)/2]}, ${itCompanies[itCompanies.length - 1]}`)
 // display each company in the array on a new line
 const displayCompanies = itCompanies.map(company => console.log(company));
