153518cd45093e922df3f15968df72e4c72e3e5a
jheave <jimenezjheave123@gmail.com>
2022-07-13 00:37:05 +0800

    refractor

--- solutions/day-01/level1.js
+++ solutions/day-01/level1.js
@@ -7,7 +7,11 @@ console.log(itCompanies);
 // display length of it companies
 console.log(`length of IT companies ${itCompanies.length}`)
 
+// display first element, middle element last element of it companies
 console.log(`${itCompanies[0]}, ${itCompanies[(itCompanies.length - 1)/2]}, ${itCompanies[itCompanies.length - 1]}`)
-// display each company in the array on a new line
+
+// display each company in the array
 const displayCompanies = itCompanies.map(company => console.log(company));
+
+// display each company in the array in uppercase
 console.log(itCompanies.map(item => item.toUpperCase()));
