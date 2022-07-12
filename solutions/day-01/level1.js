830d207b65577adc2997f63b713f27b025f18c46
jheave <jimenezjheave123@gmail.com>
2022-07-13 00:35:44 +0800

    add comment

--- solutions/day-01/level1.js
+++ solutions/day-01/level1.js
@@ -1,6 +1,7 @@
 const mixedDataTypes = [1, 'string', true, null, undefined, {}, [], () => {}];
 const itCompanies = ['Facebook', 'Google', 'Microsoft', 'Apple','IBM', 'Oracle', 'Amazon'];
 
+// display array of it companies
 console.log(itCompanies);
 console.log(`length of IT companies ${itCompanies.length}`)
 console.log(`${itCompanies[0]}, ${itCompanies[(itCompanies.length - 1)/2]}, ${itCompanies[itCompanies.length - 1]}`)
