3f05452f1ad5d8b7ca96e1ea4b7f2eff8d7d4d6e
satya <sathyagangadhara@gmail.com>
2022-07-22 11:34:23 +0530

    day-4 updated

--- solutions/day-4/src/components/colorGenComp.js
+++ solutions/day-4/src/components/colorGenComp.js
@@ -1,28 +1,26 @@
 import React from "react";
 
-
 export default function ColorGen() {
-     let str = '0123456789abcdef';
-     let hexColor = () => {
+
+     let hexcolor = () => {
           let color = '';
+          let str = '0123456789abcdef';
           for (let i = 0; i < 6; i++) {
-               let hex = Math.floor(Math.random() * str.length);
-               color += str[hex];
+               let hex = Math.floor(Math.random() * str.length );
+               color += str[hex]; 
           }
           return '#' + color;
      }
-     let style = {
-          backgroundColor: '{hexColor}',
-     }
+     let arr = Array(6).fill(0);
+
+     let sixColor = arr.map((i) => <div className="color" style={{ backgroundColor: `${hexcolor()}` }}>
+          <h1>{ hexcolor()}</h1>
+     </div>);
+     // console.log(hexcolor());
+     
      return (
           <section className="colorGen">
-               <div className="colorContainer" style={style}
-               >
-                    {hexColor()}
-               </div>
-               <button>Genarate</button>
+               {sixColor}
           </section>
      )
 }
-
-// I DO NOT KNOW WHAT I AM DONING WRONG IN MY CODE. BUT ICAN'T SEE THE CHANGE OF BACKGROUND COLOR AFTER GIVING THE HEIGHT AND WIDTH TO THE COLOR CONTAINER
