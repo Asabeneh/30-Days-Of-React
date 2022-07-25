86af48ba3c580af37895e17d329879f4be526f5e
satya <sathyagangadhara@gmail.com>
2022-07-25 17:08:39 +0530

    day-8 partially completed

---
+++ solutions/day-8/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+// import BgChange from './components/backgroundChange';
+import Country from './components/country';
+function App() {
+  return (
+    <div className="App">
+      {/* <BgChange /> */}
+      <Country />
+    </div>
+  );
+}
+
+export default App;
