3b920bd720f4a1f3b9db95b48e4ef50b48b3ec83
Pierre <piercel.coder2022@gmail.com>
2022-10-29 18:04:33 +0200

    Change some data

--- solutions/day-01/level1.js
+++ solutions/day-01/level1.js
@@ -11,3 +11,21 @@ console.log(middleItem);
 //Get the last item
 const [lastItem] = arraOfNumbers.slice(-1);
 console.log(lastItem);
+
+const mixedDataTypes = [1, true, 'Helsinki', null, undefined,1234567890123452 ]
+const lengthMixedDataTypes = mixedDataTypes.length;
+//The length should be greater than 5
+console.log(lengthMixedDataTypes);
+
+const itCompanies = ["Facebook", "Google", "Microsoft", "Apple", "IBM", "Oracle", "Amazon"];
+console.log(itCompanies);
+
+//Print the first company
+const firstCompany = itCompanies[0];
+console.log(firstCompany);
+
+const middleCompany = itCompanies[(itCompanies.length - 1) / 2];
+console.log(middleCompany);
+
+const lastCompany = itCompanies.slice(-1);
+console.log(lastCompany);
