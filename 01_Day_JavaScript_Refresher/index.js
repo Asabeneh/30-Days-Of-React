e27979823c55c1e375a2d811eba953960350cde1
abhijeet <abhijeetnishal@gmail.com>
2022-12-01 16:27:47 +0530

    Day1 commit

---
+++ 01_Day_JavaScript_Refresher/index.js
@@ -0,0 +1,17 @@
+let companiesString = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'
+const companies = companiesString.split(' ')
+console.log(companies)
+
+const arr = Array() // creates an an empty array
+console.log(arr)
+
+const eightXvalues = Array(8).fill('X') // it creates eight element values filled with 'X'
+console.log(eightXvalues) // ['X', 'X','X','X','X','X','X','X']
+
+const eight0values = Array(8).fill(0) // it creates eight element values filled with '0'
+
+const firstList = [1, 2, 3]
+const secondList = [4, 5, 6]
+const thirdList = firstList.concat(secondList)
+
+console.log(thirdList) // [1, 2, 3, 4, 5, 6]
