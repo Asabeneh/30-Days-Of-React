af2b00e57013e63ebccf8661ca551346e4fd208a
Gideon Buba <user@users-MacBook-Pro.local>
2023-09-25 00:18:57 +0100

    Started Day 1

---
+++ 01_Day_JavaScript_Refresher/introduction.js
@@ -0,0 +1,20 @@
+const popUp = document.getElementById('popUp')
+
+popUp.addEventListener('click', function() {
+    alert('Hello World')
+})
+
+
+// Arrays
+const names = Array('Gideon', 'Courage', 'Femi', 'Jude', 250, true, false);
+names[1] = 'Wife'
+let firtsName = names[0];
+const strings = " 'Gideon', 'Courage', 'Mufasa' "
+const splitstrings = strings.split('')
+
+console.log(names);
+console.log(firtsName);
+console.log(names.length);
+console.log(splitstrings);
+
+//
