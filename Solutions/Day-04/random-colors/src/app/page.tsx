bddfcded90640564ef6827b7c49885402538b157
seanj29 <seanotto29@gmail.com>
2023-10-30 13:04:58 +0000

    Day 04 partial

---
+++ Solutions/Day-04/random-colors/src/app/page.tsx
@@ -0,0 +1,16 @@
+import styles from './page.module.css'
+import RandomDiv from './RandomColorDiv'
+
+export default function Home() {
+  return (
+    <main>
+      <div>
+      <RandomDiv/>
+      <RandomDiv/>
+      <RandomDiv/>
+      <RandomDiv/>
+      <RandomDiv/>
+      </div>
+    </main>
+  )
+}
