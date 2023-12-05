2f4b94b8703e744785ee8c07422b1fc5ad96d05d
Firomsa <firomsahika2022@gmail.com>
2023-12-05 05:20:22 -0800

    exercise level 2 complete

---
+++ solutions/day-01/level2/countries.js
@@ -0,0 +1,15 @@
+const countries = [
+    'Albania',
+    'Bolivia',
+    'Canada',
+    'Denmark',
+    'Ethiopia',
+    'Finland',
+    'Germany',
+    'Hungary',
+    'Ireland',
+    'Japan',
+    'Kenya',
+  ]
+
+  export default countries;
