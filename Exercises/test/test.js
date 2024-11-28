888772ec4a3587ee2e4284408e7430c616d37644
Fitsumhelina <permin1501424@gmail.com>
2024-11-28 20:26:58 +0300

    test file addded

--- Exercises/test/test.js
+++ Exercises/test/test.js
@@ -1,7 +1,34 @@
-const rectangle = {
-  width: 20,
-  height: 10,
-}
+// const arr = ['Finland', 'Estonia', 'Sweden', 'Norway']
+// const arr2 = []
+// arr.forEach((country) => arr2.push(country.toUpperCase()))
+// console.log(arr2)
 
-let { width, height, perimeter = 200 } = rectangle
-console.log(width, height, perimeter) 
+
+// const arr = [1,2,3,4,5]
+// let sum = 0
+
+// const add = (num,i,arr) =>{
+//   sum+=num
+// }
+// arr.forEach(add)
+// console.log(sum)
+
+// const hey = () => {
+//   console.log('hey')
+// }
+// function hello() {
+//   console.log('hello')
+// }
+// hello()
+// hey()
+
+// const countries = ['Finland', 'Estonia', 'Sweden', 'Norway']
+// console.log(countries.map((country) => country.toUpperCase()))
+// const newarr = []
+// countries.forEach(x => {
+//   if (x.includes('land')) {
+//     newarr.push(x)
+//   }
+  
+// });
+// console.log(newarr)
